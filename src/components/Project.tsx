03f216a7b94f3af2aca52bf5574ed7ab1e75e70b
rbmpsh <rbmpsh@gmail.com>
2024-11-08 17:09:18 +0800

    fix: font-size

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -4,8 +4,8 @@ export const Project: React.FC = () => {
   const { project, company, region, mainInvester } = useAppSelector((state) => state.data);
   return (
     <div className="page-rounded page-bd page-bg-white p-sm-4 p-3 d-flex flex-column gap-3">
-      <div className="fw-bold page-text-xx-large">{project}</div>
-      <div className="page-text-large">{company}</div>
+      <div className="fw-bold page-text-xxx-large">{project}</div>
+      <div className="page-text-x-large">{company}</div>
       <div>
         <span className="d-lg-inline d-flex align-items-center align-self-stretch gap-1 me-3">
           <span className="material-symbols-outlined me-1">location_on</span>
